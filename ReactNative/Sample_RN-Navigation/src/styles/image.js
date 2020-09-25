9e8cee3af2eb44174412da22403a5a3959a5e33f
isumizumi <isumi.karina72@gmail.com>
2020-09-25 13:11:56 +0700

    update

--- ReactNative/Sample_RN-Navigation/src/styles/image.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const CustomImage = styled.Image`
-  width: 100px;
-  height: 100px;
-  border-color: ${props => props.theme.colors.light || 'white'};
-  border-width: 2px;
-  border-radius: 50px;
-`
--- ReactNative/Sample_RN-Navigation/src/styles/wrapper.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const Container = styled.View`
-  flex: 1;
-  margin: 16px;
-  align-items: center;
-  justify-content: center;
-`
